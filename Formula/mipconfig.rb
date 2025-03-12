478e06c024160c2e52a839c91be5f79f9d572306
ashcastle <ashcastle@users.noreply.github.com>
2025-03-13 00:10:17 +0900

    fix: Changed brew shasum

--- Formula/mipconfig.rb
+++ Formula/mipconfig.rb
@@ -1,8 +1,8 @@
 class Mipconfig < Formula
     desc "간결한 네트워크 정보를 제공하는 ipconfig 도구 (macOS용)"
     homepage "https://github.com/ashcastle/homebrew-mip"
-    url "https://github.com/ashcastle/homebrew-mip/archive/v1.0.0-beta.tar.gz"
-    sha256 "53c7d288486f7982d8342099c2aa1e61a49c3edf3f2109c4fe93eb40a98efd65"
+    url "https://github.com/ashcastle/homebrew-mip/archive/v1.0.1-beta.tar.gz"
+    sha256 "8e4453405388f7a912c34a6daf4499f2b0bdfec7af76bde44c926b83a90db2be"
     license "GPL-2.0"
   
     depends_on "go" => :build
